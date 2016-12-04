1225d1a73881bbcf99afd36cf183cb62c85f1d66
Piotrek <piotrekk1@outlook.com>
2016-12-04 15:49:42 +0100

    Dodano testy na łączenie selektorów

--- Tests/HtmlSelectorsTest.cs
+++ Tests/HtmlSelectorsTest.cs
@@ -51,5 +51,31 @@ namespace Tests
             Assert.AreEqual(1, foundObjs.Count);
             Assert.AreEqual(foundObjs.FirstOrDefault(), exampleObj);
         }
+
+        [TestMethod]
+        public string MergeTwoSelectors()
+        {
+            LoadPage();
+            HtmlNode obj1 = htmlDoc.DocumentNode.Descendants().Where(x => x.Attributes["href"] != null ? x.Attributes["href"].Value == "http://go.microsoft.com/fwlink/?LinkID=615541" : false).FirstOrDefault();
+            HtmlNode obj2 = htmlDoc.DocumentNode.Descendants().Where(x => x.Attributes["href"] != null ? x.Attributes["href"].Value == "http://go.microsoft.com/fwlink/?LinkID=615530" : false).FirstOrDefault();
+
+            List<string> selector1 = ObjectToSelector.GenerateSelector(obj1, new List<string>());
+            List<string> selector2 = ObjectToSelector.GenerateSelector(obj2, new List<string>());
+
+            List<string> result = SelectorsMerge.GenerateSelectorSchema(selector1, selector2);
+            string resultAsString = Converters.SelectorAsListToString(result);
+
+            Assert.AreEqual("#main > div:nth-child(2) > ul:nth-child(1) > li:nth-child({x}) > a:nth-child(1)", resultAsString);
+
+            return resultAsString;
+        }
+
+        [TestMethod]
+        public void ResultOfMergedSelector()
+        {
+            string mergedselector = MergeTwoSelectors();
+            List<HtmlNode> objs = SelectorToObject.FindBySelector(htmlDoc.DocumentNode, mergedselector);
+            Assert.AreEqual(12, objs.Count);
+        }
     }
 }
